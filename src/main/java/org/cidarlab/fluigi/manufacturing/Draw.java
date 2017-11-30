36940b29a3303888c3c3b7811bc6176ac008b2c9
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2017-11-29 20:57:05 -0500

    Moved draw function to dummy component

--- src/main/java/org/cidarlab/fluigi/manufacturing/Draw.java
+++ src/main/java/org/cidarlab/fluigi/manufacturing/Draw.java
@@ -1,10 +1,14 @@
 package org.cidarlab.fluigi.manufacturing;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class Draw {
 
-    public void draw (String macroname, Graphics2D canvas){
+    private HashMap<String, Object> drawparams;
+
+    public void draw(String macroname, Graphics2D canvas, HashMap<String, Object> params){
+        this.drawparams = params;
         switch (macroname){
             case "Port":
                 drawPort(canvas);
@@ -13,18 +17,22 @@ public class Draw {
                 drawMixer(canvas);
                 break;
             default:
-                throw new UnsupportedOperationException("Need to implement the drawfunction");
+                drawDummyComponent(canvas);
         }
     }
 
     private void drawMixer(Graphics2D canvas) {
-        throw new UnsupportedOperationException();
+        drawDummyComponent(canvas);
     }
 
     private void drawPort(Graphics2D canvas) {
-        throw new UnsupportedOperationException();
+        drawDummyComponent(canvas);
 
+    }
 
+    private void drawDummyComponent(Graphics2D canvas){
+        //TODO: Draw dummy component
+        throw new UnsupportedOperationException("Figure out dummy draw");
     }
 
 }
